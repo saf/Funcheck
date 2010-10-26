2745a720ed93754d1d7fb9c0b34133cdc811329c
saf <saf@aleph.(none)>
2010-10-26 20:33:05 +0200

    Prezentacja: LockTest - przyklad bledu dla istniejacego checkera; drobne techniczne w prezentacji.

---
+++ checkers/examples/LockTest.java
@@ -0,0 +1,22 @@
+import checkers.lock.quals.*;
+
+public class LockTest {
+    
+    private String password;
+    @GuardedBy("this.password")
+    private int passwordHash;
+
+    @Holding("this.password")
+    private void doChangePassword(String p) {
+	password = p;
+	passwordHash = p.hashCode();
+    }
+
+    public int getPasswordHash() {
+	return this.passwordHash;
+    }
+    
+    public void changePassword(String p) {
+	doChangePassword(p);
+    }
+}
