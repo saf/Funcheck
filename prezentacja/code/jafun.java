08be009e1539547fff218ee07242cf3686c7d850
Sławomir Rudnicki <sr248277@students.mimuw.edu.pl>
2010-10-26 12:42:58 +0200

    Prezentacja: JaFun, dowodzenie i projekt checkera.

---
+++ prezentacja/code/jafun.java
@@ -0,0 +1,13 @@
+immutable class FuncInteger {
+  rep Integer value;
+  
+  lstate FuncInteger(Integer v) {
+    this.value = v;
+  }
+    
+  func Integer cubed() {
+    let Integer square = this.value * this.value
+    in
+      sq * this.value
+  }
+}
