c7d3e3509088f670279d4bd3ff18a5d3da96d13c
saf <saf@aleph.(none)>
2011-04-01 20:06:37 +0200

    Dataflow analysis for this references
    Implemented a simple dataflow analysis which effectively tracks
    this references, using the built-in Flow mechanism. Needs testing.

---
+++ checkers/examples/checkers/fun/examples/FlowTest.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package checkers.fun.examples;
+
+import checkers.fun.quals.Anonymous;
+
+/**
+ *
+ * @author saf
+ */
+public class FlowTest {
+
+    public FlowTest process(FlowTest c) {
+        
+        FlowTest z;
+        if (false) {
+            z = this;
+        } else {
+            z = c;
+        }
+        z.bar(0);
+        return this;
+    }
+
+    @Anonymous
+    public FlowTest foo(Integer c) {
+        return new FlowTest();
+    }
+
+    public FlowTest bar(Integer c) {
+        return new FlowTest();
+    }
+
+}
---
+++ checkers/src/checkers/fun/quals/MaybeThis.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package checkers.fun.quals;
+
+import checkers.quals.SubtypeOf;
+
+/**
+ *
+ * @author saf
+ */
+@SubtypeOf({})
+public @interface MaybeThis {}
