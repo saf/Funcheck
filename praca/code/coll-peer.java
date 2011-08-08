0f3ef6f04dd6dbba73621ff87fee7e23c4437bb8
Sławek Rudnicki <sr248277@students.mimuw.edu.pl>
2011-08-08 11:51:03 +0200

    Thesis: corrections and improvements

--- praca/code/coll-peer.java
+++ praca/code/coll-peer.java
@@ -2,21 +2,16 @@ public class PeerStack<T> {
     private static class Cell<CT> {
         private @Peer CT data;
         private @Immutable @Peer Cell next;
-
         @Anonymous
         public Cell(@Peer CT data, @Immutable @Peer Cell next) {
             this.data = data;
             this.next = next;
         }
-
         @ReadOnly public @Peer CT getData() { return data; }
         @ReadOnly public @Immutable @Peer Cell getNext() { return next; }
     }
-
     private @Immutable @Peer Cell<T> head;
-
     public PeerStack() { head = null; }
-
     public @Peer T pop() {
         if (head == null) return null;
         else {
